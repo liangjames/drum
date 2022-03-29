d467ddda8d4a65b346e7297dff87172c57bfe232
liangjames <46747869+liangjames@users.noreply.github.com>
2022-03-29 17:28:23 +0200

    Update index.js
    even better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  document.querySelectorAll(".drum")[i].addEventListener("mousedown", function() {
 
     playSound(this.innerHTML);
 
